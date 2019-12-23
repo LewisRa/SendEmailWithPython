3f091108b57d8fc38884ffc573a06b8e01940c58
LewisRa <rachel.lewis9312@gmail.com>
2019-12-23 18:51:55 -0500

    change pasword and user name

--- emailDistributionList.py/emailDistributionList.py
+++ emailDistributionList.py/emailDistributionList.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import smtplib,ssl
+import getpass
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import date
 
 today = date.today()
 contacts=pd.read_excel('emails.xlsx', sheet_name='Sheet1')
-password =mycred()[1]
+password =  getpass("Please enter the email password:   ")
 subject = "Test"
 for i in range(len(contacts)):
     name, email, address = contacts.iloc[i]
     port=465
     smtp_server ='smtp.gmail.com'
     msg=MIMEMultipart()
-    msg['From']= mycred()[0]
+    msg['From']= 'rachel.lewis9312@gmail.com' #mycred()[0]
     msg['To']= email
     msg['Subject']= subject + "_" + str(today)
     body = "Hey {}, how is it going? I wanted to confirm your information. Are you still at {}?".format(name.split()[0], address)
@@ -22,7 +23,7 @@ for i in range(len(contacts)):
     text = msg.as_string()
     context=ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(mycred()[0], password)
-        server.sendmail(mycred()[0], msg['To'], text)
+        server.login('rachel.lewis9312@gmail.com', password)
+        server.sendmail('rachel.lewis9312@gmail.com', msg['To'], text)
     print("Sent to:", name)
 print('Done')
