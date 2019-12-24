3c843796d379f8ff520fef335d6ee8f34a1a32b3
LewisRa <rachel.lewis9312@gmail.com>
2019-12-23 20:27:30 -0500

    Created config file and .gitignore file

--- emailDistribution/emailDistributionList.py
+++ emailDistribution/emailDistributionList.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import smtplib,ssl
+import os
 import getpass
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import date
+import config
+
+
 
 today = date.today()
 contacts=pd.read_excel('emails.xlsx', sheet_name='Sheet1')
-password =  getpass("Please enter the email password:   ")
+username = config.username
+password = config.password
 subject = "Test"
 for i in range(len(contacts)):
     name, email, address = contacts.iloc[i]
     port=465
     smtp_server ='smtp.gmail.com'
     msg=MIMEMultipart()
-    msg['From']= 'rachel.lewis9312@gmail.com' #mycred()[0]
+    msg['From']= username #mycred()[0]
     msg['To']= email
     msg['Subject']= subject + "_" + str(today)
     body = "Hey {}, how is it going? I wanted to confirm your information. Are you still at {}?".format(name.split()[0], address)
@@ -23,7 +28,7 @@ for i in range(len(contacts)):
     text = msg.as_string()
     context=ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login('rachel.lewis9312@gmail.com', password)
-        server.sendmail('rachel.lewis9312@gmail.com', msg['To'], text)
+        server.login(username, password)
+        server.sendmail(username, msg['To'], text)
     print("Sent to:", name)
 print('Done')
